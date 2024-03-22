1c38431571b569e4ce638f3d1a26adaea7468620
Nyc27 <156249960+Nyc27@users.noreply.github.com>
2024-03-22 12:01:38 +0100

    script.js

--- script.js
+++ script.js
@@ -11,3 +11,44 @@ mobileToggle.addEventListener('click', function () {
     })
 
 })
+
+const tendinaButtons = document.querySelectorAll('.tendina-button');
+ 
+
+    tendinaButtons.forEach(tendinaButton => {
+        tendinaButton.addEventListener('click', function () {
+        const tendina = this.querySelector('.tendina');
+        tendina.classList.toggle('show-mobile');
+    });
+
+        tendinaButton.addEventListener('mouseenter', function (){
+        const tendina = this.querySelector('.tendina');
+        tendina.classList.add('show-desktop')
+    });
+
+
+
+    tendinaButton.addEventListener('mouseleave', function () {
+        const tendina = this.querySelector('.tendina');
+        // Avvia un timer di 1 secondo per rimuovere la classe dopo 1 secondo
+        timeoutId = setTimeout(() => {
+            tendina.classList.remove('show-desktop');
+        }, 100);
+    });
+
+    // Cancella il timer se il mouse rientra nell'elemento prima che scada il timer
+    tendinaButton.addEventListener('mouseenter', function () {
+        clearTimeout(timeoutId);
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
